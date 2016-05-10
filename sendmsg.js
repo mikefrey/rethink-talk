7a0a4ee8291c62815f89f2e71172d67746f55ac5
Mike Frey <frey.mike@gmail.com>
2016-05-10 18:40:52 -0500

    send message with user

--- sendmsg.js
+++ sendmsg.js
@@ -1,14 +1,33 @@
-const ip = process.argv[2]
-const msg = process.argv[3]
+var host = 'localhost'
+var usr = process.argv[2]
+var msg = process.argv[3]
 
-const r = require('rethinkdb')
-const connOpts = { host: 'localhost', db: 'nodemn' }
+var r = require('rethinkdb')
+var connOpts = { host: host, db: 'nodemn' }
 
 r.connect(connOpts).then(function(conn) {
-  r.table('messages')
-    .insert({ msg: msg })
-    .run(conn)
-    .then(function(res) {
-      conn.close()
+
+  r.table('users').filter({ username: usr }).nth(0).run(conn)
+    .then(function(user) {
+      console.log(user)
+      if (user) return user
     })
+    .catch(function(err) {
+      console.log('caught')
+      return r.table('users')
+        .insert({ username: usr }, {returnChanges:true}).run(conn)
+        .then(function(res) {
+          return res.changes[0].new_val
+        })
+    })
+    .then(function(user) {
+
+      r.table('messages')
+        .insert({ userID: user.id, msg: msg })
+        .run(conn)
+        .then(function(res) {
+          conn.close()
+        })
+    })
+
 })
